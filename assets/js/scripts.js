d29fe52445f819e63fb4cd4c9419f8fd6811b764
Lourdes Roashan <root.roashan@gmail.com>
2018-09-18 16:13:45 -0300

    Updated Resume

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,11 +1,8 @@
-
-
-
 var typed3 = new Typed('.typed', {
-    strings: ['Hi, I\'m Lourdes'],
-    typeSpeed: 30,
-    smartBackspace: true,
-    cursorChar: "<span class='type-cursor'>_</b>",
-    autoInsertCss: true,
-    loop: false
-  });
+  strings: ['Hi, I\'m Lourdes'],
+  typeSpeed: 30,
+  smartBackspace: true,
+  cursorChar: "<span class='type-cursor'>_</b>",
+  autoInsertCss: true,
+  loop: false
+});
